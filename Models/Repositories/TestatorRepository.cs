101a4df8458310fb388843367d8ab7a1393b066e
fredforrest <144379743+fredforrest@users.noreply.github.com>
2024-11-12 21:47:56 +0100

    opret, gem, vis, slet arvelader

---
+++ Models/Repositories/TestatorRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace Arvefordeleren.Models.Repositories
+{
+    public static class TestatorRepository
+    {
+        private static List<Testator> testators = new List<Testator>();
+        
+
+
+        public static void AddNewTestator(Testator testator)
+        {
+            
+            testators.Add(testator);
+        }
+
+        public static Testator GetTestatorByName(string name) => testators.FirstOrDefault(t => t.Name == name);
+
+        public static void DeleteTestator(string name)
+        {
+            var testator = GetTestatorByName(name);
+            if (testator != null)
+            {
+                testators.Remove(testator);
+            }
+        }
+
+        public static List<Testator> GetTestators() => new List<Testator>(testators);
+
+
+
+
+    }
+}
---
+++ Models/Testator.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+
+namespace Arvefordeleren.Models
+{
+    public class Testator
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; } = null;
+
+        public string? Address { get; set; } = null;
+                
+
+    }
+}
