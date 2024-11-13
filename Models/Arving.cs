a57f891a0739858a96cd41e4f96d49dd5c7b5205
L3wis76 <143950236+L3wis76@users.noreply.github.com>
2024-11-13 19:28:55 +0100

    Indtast af arving og gemt til listen

---
+++ Models/Arving.cs
@@ -0,0 +1,14 @@
+
+namespace Arvefordeleren.Models
+{
+    public class Arving
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+
+    public enum ArvingType
+{
+    Friarv, tvangsarv
+}
+}
---
+++ Models/Repositories/ArvingerRepository.cs
@@ -0,0 +1,18 @@
+
+namespace Arvefordeleren.Models.Repositories
+{
+    public class ArvingerRepository
+    {
+        private static List<Arving> _arvinger = new List<Arving>();
+
+        public static void AddArving(Arving arving)
+        {
+            _arvinger.Add(arving);
+        }
+
+        public static List<Arving> GetArvinger()
+        {
+            return _arvinger;
+        }
+    }
+}
