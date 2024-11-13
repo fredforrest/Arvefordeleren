f77eb79317e27ef64bd3bb6f2f746f7a7a5f9db8
Milad Fahham <miladfahham@gmail.com>
2024-11-13 16:11:25 +0100

    Lavet metode til at hente aktiv fra liste i aktivRepo.

--- Models/Repositories/AssetsRepository.cs
+++ Models/Repositories/AssetsRepository.cs
@@ -3,4 +3,9 @@
 public static class AssetsRepository
 {
     public static List<Asset> Assets { get; set; } = new List<Asset>();
+
+    public static Asset? GetAssetById(int id)
+    {
+        return Assets.FirstOrDefault(a => a.Id == id);
+    }
 }
