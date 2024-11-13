3ee37ad3d3bfa541fe0e181c2d17b9d51517aa84
Milad Fahham <miladfahham@gmail.com>
2024-11-13 15:57:07 +0100

    Metode til at fjerne aktiv i arvRepo er oprettet.

--- Models/Repositories/AssetsRepository.cs
+++ Models/Repositories/AssetsRepository.cs
@@ -3,4 +3,14 @@
 public static class AssetsRepository
 {
     public static List<Asset> Assets { get; set; } = new List<Asset>();
+
+    public static void RemoveAsset(int id)
+    {
+        Asset asset = Assets.FirstOrDefault(a => a.Id == id);
+
+        if (asset != null)
+        {
+            Assets.Remove(asset);
+        }
+    }
 }
