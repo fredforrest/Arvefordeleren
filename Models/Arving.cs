e899d06b384b2301ecb6d6a27816eac3091858ab
TheAltintas <Altintas12@outlook.dk>
2024-11-16 12:23:04 +0100

    Nå adresse skulle ikke være med så den har jeg fjernet nicklas du må gerne fikse mere styling hahah

--- Models/Arving.cs
+++ Models/Arving.cs
@@ -3,7 +3,6 @@ namespace Arvefordeleren.Models
     public class Arving
     {
         public string Name { get; set; }
-        public string Address { get; set; }
         public ArvingType Type { get; set; }
     }
 
