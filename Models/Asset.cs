9afd66aa2ef5c47954343f133cc76df596e82535
Milad Fahham <miladfahham@gmail.com>
2024-11-22 17:38:50 +0100

    Liste for aktiv og arving delvis implementeret. Styling justeret og ikon logik implmenteret på aktiv klassen

--- Models/Asset.cs
+++ Models/Asset.cs
@@ -4,8 +4,29 @@ public class Asset
 {
     public int Id { get; set; }
     public AssetType Type { get; set; }
-    public string? Note { get; set; } 
-    
+    public string? Note { get; set; }
+
+    public string Icon 
+    {
+        get
+        {
+            switch (Type)
+            {
+                case AssetType.Køretøj:
+                    return "/images/Car.png";
+
+                case AssetType.Bolig:
+                    return "/images/home.png";
+
+                case AssetType.Værdigenstand:
+                    return "/images/valuables.png";
+
+                default:
+                    return "/images/favicon.png";
+            }
+        }    
+    }
+
     public Asset() {}
 
     public Asset(AssetType type)
