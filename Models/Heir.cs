b8e020d3e4c19aa6bd55a22d73e1a29281854458
TheAltintas <Altintas12@outlook.dk>
2024-11-24 19:24:23 +0100

    Har tilføjet billeder til Arving, så man inde på Rapportsiden har lidt mere at kigge på og det måske er mere "Brugervenligt"

--- Models/Heir.cs
+++ Models/Heir.cs
@@ -8,7 +8,35 @@ namespace Arvefordeleren.Models
         public string Name { get; set; }
         public RelationType Relation { get; set; }
 
+        public string Icon
+        {
+            get
+            {
+                switch (Relation)
+                {
+                    case RelationType.Barn:
+                        return "/images/Barn.png";
+
+                    case RelationType.Barnebarn:
+                        return "/images/Barnebarn.png";
+
+                    case RelationType.Forældre:
+                        return "/images/Forældre.png";
+
+                    case RelationType.Bedsteforældre:
+                        return "/images/Bedsteforældre.png";
+
+                    case RelationType.Andet:
+                        return "/images/Andet.png";
+
+                    default:
+                        return "/images/favicon.png";
+                }
+            }
+        }
     }
+
+
     public enum RelationType
     {
        Barn, Barnebarn, Forældre, Bedsteforældre, Andet
