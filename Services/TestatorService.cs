86c7ea5c57d16465fa42df5b3fe94baa6348ed97
Milad Fahham <miladfahham@gmail.com>
2024-12-05 18:07:34 +0100

    Merged develop into branch

--- Services/TestatorService.cs
+++ Services/TestatorService.cs
@@ -1,6 +1,39 @@
-﻿namespace Arvefordeleren.Services
+﻿using Arvefordeleren.Models;
+using Arvefordeleren.Models.Repositories;
+
+namespace Arvefordeleren.Services
 {
     public class TestatorService
     {
+        public void EstablishRelationToHeir(Heir heir, int selectedTestatorId)
+        {
+            if (heir.TypeOfChild == TypeOfChild.Fællesbarn)
+            {
+                foreach (var testator in TestatorRepository.testators)
+                {
+                    if (!testator.Heirs.Any(h => h.Name == heir.Name && h.Relation == heir.Relation))
+                    {
+                        testator.Heirs.Add(heir);
+                    }
+                }
+            }
+            else if (selectedTestatorId != 0)
+            {
+                var oldTestator = TestatorRepository.testators.FirstOrDefault(t => t.Heirs.Any(h => h.Name == heir.Name && h.Relation == heir.Relation));
+
+                if (oldTestator != null)
+                {
+                    var heirToRemove = oldTestator.Heirs.FirstOrDefault(h => h.Name == heir.Name && h.Relation == heir.Relation);
+                    oldTestator.Heirs.Remove(heirToRemove);
+                }
+
+                var newTestator = TestatorRepository.testators.FirstOrDefault(t => t.Id == selectedTestatorId);
+
+                if (newTestator != null && !newTestator.Heirs.Any(h => h.Name == heir.Name && h.Relation == heir.Relation))
+                {
+                    newTestator.Heirs.Add(heir);
+                }
+            }
+        }
     }
 }
